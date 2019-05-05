4b14f2da517c5d1d773676b903c97e0ae92e22ad
haydencrain <haydencrain@optusnet.com.au>
2019-05-06 00:16:34 +1000

    HELPS-141 implement students store

---
+++ packages/application/store/students/methods.js
@@ -0,0 +1,10 @@
+import { storeModule } from '../../core/helpers'
+
+const MODULE = 'students'
+export const REQUEST = `request`
+export const SUCCESS = `success`
+export const ERROR = `error`
+export const STUDENTS = 'users'
+export const CLEAR = 'clear'
+
+export const studentsModule = method => storeModule(MODULE, method)
