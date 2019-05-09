994290ec27c360df7ff887a8f4673f7dcd744ca4
Hayden Crain <haydencrain@optusnet.com.au>
2019-05-09 15:03:31 +1000

    HELPS-703 move application to root folder, remove lame ass lerna (may add back later idk)

---
+++ components/AdminDashboard/AppSidebar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="sidebar">
+    <div class="logo">logo</div>
+    <v-list>
+      <v-list-tile>
+        <v-list-tile-action>
+          <v-icon>dashboard</v-icon>
+        </v-list-tile-action>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-action>
+          <v-icon>dashboard</v-icon>
+        </v-list-tile-action>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-action>
+          <v-icon>dashboard</v-icon>
+        </v-list-tile-action>
+      </v-list-tile>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ layouts/admin.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-app class="app">
+    <AppSidebar class="column left-column" />
+    <div class="column right-column">
+      <v-toolbar flat />
+      <div class="content">
+        <div class="sidebar"></div>
+        <v-content>
+          <nuxt />
+        </v-content>
+      </div>
+    </div>
+  </v-app>
+</template>
+
+<script>
+import AppSidebar from '../components/AdminDashboard/AppSidebar'
+
+export default {
+  components: { AppSidebar },
+  data() {
+    return {
+      drawer: true,
+      mini: true
+    }
+  },
+  mounted() {
+    if (process.browser) {
+      screen.orientation.lock('portrait')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~/assets/styles/global.scss';
+
+.column {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+.left-column {
+  background-color: aqua;
+}
+
+.right-column {
+  background-color: aquamarine;
+  flex: 1 1 auto;
+}
+
+.app {
+  .application--wrap {
+    flex-direction: unset;
+  }
+}
+
+// .sidebar {
+//   background-color: black;
+//   height: 100vh;
+//   min-width: 55px;
+// }
+
+// .header-and-content {
+//   background: green;
+//   width: 100%;
+// }
+
+// .header {
+//   background-color: red;
+//   height: 100px;
+// }
+
+// .content {
+//   background: orange;
+//   display: flex;
+//   flex-direction: row;
+// }
+
+// .collapsible-sidebar {
+//   height: 100%;
+//   background-color: gray;
+//   width: 240px;
+// }
+</style>
