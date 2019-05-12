69a80097bb4296ef916ef564c17d4674e063169a
sheng zeng <bahtmannn@gmail.com>
2019-05-12 15:06:51 +1000

    added test flow for student and admin

--- features/login_auth.feature
+++ features/login_auth.feature
@@ -1,16 +1,22 @@
-Feature: Student Login
+Feature: User Login
 
-  Test for successful student login
+  Test for successful user login
 
 # Background: student credentials 
 # Given a student with username "username" and password "password" and is registered
 
-  @studentHook
-Scenario: student logs in with correct credentials 
-    Given the student is on the login page
+  @userHook
+Scenario Outline: user logs in with correct credentials 
+    Given the user is on the login page
     And is not logged in
-    When the student enters "12345678" AND "password" AND submits
+    When the user enters "<username>" AND "<password>" AND submits
     Then they are logged in and sent to the dashboard
+
+Examples:
+| username | password |
+| 12345678 | password |
+| 11230099 | password |
+
 
 # Scenario: user logs in for the first time 
 # Given the user is on the login page
--- features/step_definitions/loginAuth.js
+++ features/step_definitions/loginAuth.js
@@ -6,11 +6,19 @@ const Selector = (input, t) => {
 }
 const getLocation = ClientFunction(() => document.location.href)
 
-Before('@studentHook', async () => {
+Before('@userHook', async () => {
   console.log('Running student login test')
 })
+//test to see if the user is student or admin
+function testIdentity(username, password) {
+  if (username == '123456789' && password == 'password') {
+    return 'student'
+  } else if (username == '11230099' && password == 'password') {
+    return 'admin'
+  }
+}
 //navigate to index
-Given('the student is on the login page', async t => {
+Given('the user is on the login page', async t => {
   await t.navigateTo('http://localhost:3000')
 })
 //clear cookies
@@ -25,12 +33,13 @@ Given('is not logged in', async t => {
 })
 //test for username and password
 When(
-  /^the student enters "(.+)" AND "(.+)" AND submits$/,
+  /^the user enters "(.+)" AND "(.+)" AND submits$/,
   async (t, [username, password]) => {
     const usernameField = Selector('#username', t)
     const passwordField = Selector('#password', t)
     const submitBtn = Selector('#submitBtn', t)
 
+    this.user = testIdentity(username, password)
     await t
       .click(usernameField)
       .pressKey('ctrl+a delete')
@@ -41,9 +50,15 @@ When(
       .click(submitBtn)
   }
 )
-
+//test for admin or student dashboard
 Then('they are logged in and sent to the dashboard', async t => {
-  await t
-    .expect(getLocation())
-    .contains('http://localhost:3000/student/dashboard')
+  if (this.user == 'student')
+    await t
+      .expect(getLocation())
+      .contains('http://localhost:3000/student/dashboard')
+  else if (this.user == 'admin') {
+    await t
+      .expect(getLocation())
+      .contains('http://localhost:3000/admin/dashboard')
+  }
 })
