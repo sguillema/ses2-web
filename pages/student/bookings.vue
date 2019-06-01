f20898377c45631fb281ecaf176b1f3e096ff6fe
adibmkhan <37283920+adibmkhan@users.noreply.github.com>
2019-06-01 15:50:02 +1000

    - Formatted Alignment of My Bookings items - Opening up dialog by clicking "Register Skillset" - Adding stepper

--- pages/student/bookings.vue
+++ pages/student/bookings.vue
@@ -1,7 +1,64 @@
 <template>
   <div id="page-dataTable">
     <section class="container">
-      <v-btn color="error" style="float:right;">Add Session</v-btn>
+      <v-layout style="float:right">
+        <v-btn color="primary" @click.stop="dialog = true">
+          Register Skillset
+        </v-btn>
+
+        <v-dialog v-model="dialog" max-width="1100" max-height="406">
+          <v-card>
+            <v-stepper v-model="e1">
+              <v-stepper-header>
+                <h1 class="dialog-title">HELPS Workshop Booking</h1>
+                <v-stepper-step :complete="e1 > 1" step="1">
+                  Step 1
+                </v-stepper-step>
+
+                <v-divider />
+
+                <v-stepper-step :complete="e1 > 2" step="2">
+                  Step 2
+                </v-stepper-step>
+
+                <v-divider />
+
+                <v-stepper-step step="3">Step 3</v-stepper-step>
+              </v-stepper-header>
+
+              <v-stepper-items>
+                <v-stepper-content step="1" class="step-container">
+                  <v-btn color="primary" @click="e1 = 2">Next</v-btn>
+
+                  <v-btn flat>Cancel</v-btn>
+                </v-stepper-content>
+
+                <v-stepper-content step="2">
+                  <v-card class="mb-5" color="grey lighten-1" height="200px" />
+
+                  <v-btn color="primary" style="float:left" @click="e1 = 3">
+                    Next
+                  </v-btn>
+
+                  <v-btn flat>Cancel</v-btn>
+                </v-stepper-content>
+
+                <v-stepper-content step="3">
+                  <v-card class="mb-5" color="grey lighten-1" height="200px" />
+
+                  <v-btn color="primary" @click="e1 = 1">Continue</v-btn>
+
+                  <v-btn flat>Cancel</v-btn>
+                </v-stepper-content>
+              </v-stepper-items>
+            </v-stepper>
+
+            <v-card-actions>
+              <v-spacer />
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-layout>
       <h1>My Bookings</h1>
       <v-toolbar flat color="white">
         <v-toolbar-title>Your Bookings</v-toolbar-title>
@@ -14,19 +71,22 @@
       >
         <template v-slot:items="props">
           <td>{{ props.item.name }}</td>
-          <td class="text-xs-right">{{ props.item.type }}</td>
-          <td class="text-xs-right">{{ props.item.time }}</td>
-          <td class="text-xs-right">{{ props.item.room }}</td>
-          <td class="text-xs-right">{{ props.item.advisor }}</td>
-          <td class="text-xs-right">{{ props.item.type2 }}</td>
-          <td class="text-xs-right">{{ props.item.icon }}</td>
-          <td><v-checkbox v-model="props.selected" /></td>
-          <td><i class="fa fa-angle-down" style="font-size:20px"></i></td>
+          <td class="text-xs-left">{{ props.item.type }}</td>
+          <td class="text-xs-left">{{ props.item.time }}</td>
+          <td class="text-xs-left">{{ props.item.room }}</td>
+          <td class="text-xs-left">{{ props.item.advisor }}</td>
+          <td class="text-xs-left">{{ props.item.type2 }}</td>
+          <td class="text-xs-left">{{ props.item.icon }}</td>
+          <td>
+            <v-checkbox v-model="props.selected" />
+          </td>
+          <td>
+            <i class="fa fa-angle-down" style="font-size:20px"></i>
+          </td>
         </template>
       </v-data-table>
     </section>
     <section class="container">
-      <v-btn color="error" style="float:right;">Add Session</v-btn>
       <v-toolbar flat color="white">
         <v-toolbar-title>Past Bookings</v-toolbar-title>
       </v-toolbar>
@@ -38,14 +98,18 @@
       >
         <template v-slot:items="props">
           <td>{{ props.item.name }}</td>
-          <td class="text-xs-right">{{ props.item.type }}</td>
-          <td class="text-xs-right">{{ props.item.time }}</td>
-          <td class="text-xs-right">{{ props.item.room }}</td>
-          <td class="text-xs-right">{{ props.item.advisor }}</td>
-          <td class="text-xs-right">{{ props.item.type2 }}</td>
-          <td class="text-xs-right">{{ props.item.icon }}</td>
-          <td><v-checkbox v-model="props.selected" /></td>
-          <td><i class="fa fa-angle-down" style="font-size:20px"></i></td>
+          <td class="text-xs-left">{{ props.item.type }}</td>
+          <td class="text-xs-left">{{ props.item.time }}</td>
+          <td class="text-xs-left">{{ props.item.room }}</td>
+          <td class="text-xs-left">{{ props.item.advisor }}</td>
+          <td class="text-xs-left">{{ props.item.type2 }}</td>
+          <td class="text-xs-left">{{ props.item.icon }}</td>
+          <td>
+            <v-checkbox v-model="props.selected" />
+          </td>
+          <td>
+            <i class="fa fa-angle-down" style="font-size:20px"></i>
+          </td>
         </template>
       </v-data-table>
     </section>
@@ -53,6 +117,7 @@
 </template>
 
 <script>
+import moment from 'moment'
 import { authModule, TYPE } from '~/store/auth/methods'
 import { studentAuthenticated } from '../../middleware/authenticatedRoutes'
 
@@ -61,6 +126,9 @@ export default {
   layout: 'student',
   data() {
     return {
+      dialog: false,
+      e1: 0,
+
       type: this.$store.getters[authModule(TYPE)],
 
       headers: [
@@ -111,6 +179,12 @@ export default {
           type2: 'Part 2 of Program Name'
         }
       ]
+    }
+  },
+  methods: {
+    stepOne() {
+      alert('Sent!')
+      this.dialog = false
     }
   }
 }
