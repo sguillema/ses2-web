2173a805806a4c7bbfa9a1ecf59fce03af89c41a
Abdur Rahman Murad <abdurrahman.murad-1@student.uts.edu.au>
2019-06-01 20:54:50 +1000

    created 3 stesps. But still not done yet

--- pages/student/bookings.vue
+++ pages/student/bookings.vue
@@ -1,7 +1,127 @@
+/* eslint-disable prettier/prettier */
 <template>
   <div id="page-dataTable">
     <section class="container">
-      <v-btn color="error" style="float:right;">Add Session</v-btn>
+      <v-layout style="float:right">
+        <v-btn color="primary" style="float:right" @click.stop="dialog = true">
+          Register Skillset
+        </v-btn>
+        <v-dialog v-model="dialog" max-width="1100" max-height="300">
+          <v-card>
+            <section class="container">
+              <v-stepper v-model="e1">
+                <v-stepper-header>
+                  <h1>HELPS Workshop Booking</h1>
+                  <v-stepper-step :complete="e1 > 1" step="0">
+                    test
+                  </v-stepper-step>
+
+                  <v-stepper-step :complete="e1 > 1" step="1">
+                    Step 1
+                  </v-stepper-step>
+
+                  <v-divider />
+
+                  <v-stepper-step :complete="e1 > 2" step="2">
+                    Step 2
+                  </v-stepper-step>
+
+                  <v-divider />
+
+                  <v-stepper-step step="3">Step 3</v-stepper-step>
+                </v-stepper-header>
+                <section class="container"></section>
+
+                <v-stepper-content step="0" class="step-container">
+                  <div class="firstpage-content">
+                    <p>
+                      <b>Important Information:</b>
+                    </p>
+                    <p>
+                      <b>• Follow</b>
+                      each site complete your booking.
+                      <br />
+                      <b>• Check</b>
+                      the time to ensure that there is no timetable clash.
+                      <br />
+                      <b>• Check your email</b>
+                      (UTS email address) for the booking confirmation.
+                      <br />
+                      <b>• Cancel,</b>
+                      if no longer avaiable,
+                      <b>24 hours before</b>
+                      the scheduled session by clicking on
+                      <b>‘My Booking‘</b>
+                      tab and then the
+                      <b>‘detail’</b>
+                      link.
+                      <br />
+                      • Please know that failing to turn up for your registered
+                      workshop is not fair to those on the waiting list. Repeat
+                      offenders (2 no-shows) may be barred from registering for
+                      workshop.
+                      <br />
+                      <b>Registration is now open for 2019 Autumn workshops!</b>
+                      <br />
+                      <b>
+                        this stage, we only open registration for March and
+                        April workshops. For the remaining workshops,
+                        registration will be open in week 5.
+                      </b>
+                    </p>
+                  </div>
+                  <v-btn color="primary" @click="e1 = 1">Next</v-btn>
+                </v-stepper-content>
+
+                <v-stepper-items>
+                  <v-stepper-content step="1" class="step-container">
+                    <p>Bla Blah</p>
+
+                    <v-btn color="primary" @click="e1 = 2">Next</v-btn>
+                  </v-stepper-content>
+
+                  <v-stepper-content step="2">
+                    <!-- <v-card
+                      class="mb-5"
+                      color="grey lighten-1"
+                      height="200px"
+                    />-->
+                    <v-stepper-items>
+                      <div class="step-content">
+                        <p>
+                          <b>Important Information:</b>
+                        </p>
+                      </div>
+                    </v-stepper-items>
+
+                    <v-btn color="primary" style="float:left" @click="e1 = 3">
+                      Next
+                    </v-btn>
+
+                    <v-btn flat>Cancel</v-btn>
+                  </v-stepper-content>
+
+                  <v-stepper-content step="3">
+                    <v-card
+                      class="mb-5"
+                      color="grey lighten-1"
+                      height="200px"
+                    />
+
+                    <v-btn color="primary" @click="e1 = 0">Continue</v-btn>
+
+                    <v-btn flat>Cancel</v-btn>
+                  </v-stepper-content>
+                </v-stepper-items>
+              </v-stepper>
+
+              <v-card-actions>
+                <v-spacer />
+              </v-card-actions>
+            </section>
+          </v-card>
+        </v-dialog>
+      </v-layout>
       <h1>My Bookings</h1>
       <v-toolbar flat color="white">
         <v-toolbar-title>Your Bookings</v-toolbar-title>
@@ -14,19 +134,22 @@
       >
         <template v-slot:items="props">
           <td>{{ props.item.name }}</td>
-          <td class="text-xs-right">{{ props.item.type }}</td>
-          <td class="text-xs-right">{{ props.item.time }}</td>
-          <td class="text-xs-right">{{ props.item.room }}</td>
-          <td class="text-xs-right">{{ props.item.advisor }}</td>
-          <td class="text-xs-right">{{ props.item.type2 }}</td>
-          <td class="text-xs-right">{{ props.item.icon }}</td>
-          <td><v-checkbox v-model="props.selected" /></td>
-          <td><i class="fa fa-angle-down" style="font-size:20px"></i></td>
+          <td class="text-xs-left">{{ props.item.type }}</td>
+          <td class="text-xs-left">{{ props.item.time }}</td>
+          <td class="text-xs-left">{{ props.item.room }}</td>
+          <td class="text-xs-left">{{ props.item.advisor }}</td>
+          <td class="text-xs-left">{{ props.item.type2 }}</td>
+          <td class="text-xs-left">{{ props.item.icon }}</td>
+          <td>
+            <v-checkbox v-model="props.selected" />
+          </td>
+          <td>
+            <i class="fa fa-angle-down" style="font-size:20px"></i>
+          </td>
         </template>
       </v-data-table>
     </section>
     <section class="container">
-      <v-btn color="error" style="float:right;">Add Session</v-btn>
       <v-toolbar flat color="white">
         <v-toolbar-title>Past Bookings</v-toolbar-title>
       </v-toolbar>
@@ -38,14 +161,18 @@
       >
         <template v-slot:items="props">
           <td>{{ props.item.name }}</td>
-          <td class="text-xs-right">{{ props.item.type }}</td>
-          <td class="text-xs-right">{{ props.item.time }}</td>
-          <td class="text-xs-right">{{ props.item.room }}</td>
-          <td class="text-xs-right">{{ props.item.advisor }}</td>
-          <td class="text-xs-right">{{ props.item.type2 }}</td>
-          <td class="text-xs-right">{{ props.item.icon }}</td>
-          <td><v-checkbox v-model="props.selected" /></td>
-          <td><i class="fa fa-angle-down" style="font-size:20px"></i></td>
+          <td class="text-xs-left">{{ props.item.type }}</td>
+          <td class="text-xs-left">{{ props.item.time }}</td>
+          <td class="text-xs-left">{{ props.item.room }}</td>
+          <td class="text-xs-left">{{ props.item.advisor }}</td>
+          <td class="text-xs-left">{{ props.item.type2 }}</td>
+          <td class="text-xs-left">{{ props.item.icon }}</td>
+          <td>
+            <v-checkbox v-model="props.selected" />
+          </td>
+          <td>
+            <i class="fa fa-angle-down" style="font-size:20px"></i>
+          </td>
         </template>
       </v-data-table>
     </section>
@@ -53,6 +180,7 @@
 </template>
 
 <script>
+import moment from 'moment'
 import { authModule, TYPE } from '~/store/auth/methods'
 import { studentAuthenticated } from '../../middleware/authenticatedRoutes'
 
@@ -61,6 +189,9 @@ export default {
   layout: 'student',
   data() {
     return {
+      dialog: false,
+      e1: 0,
+
       type: this.$store.getters[authModule(TYPE)],
 
       headers: [
@@ -111,6 +242,12 @@ export default {
           type2: 'Part 2 of Program Name'
         }
       ]
+    }
+  },
+  methods: {
+    stepOne() {
+      alert('Sent!')
+      this.dialog = false
     }
   }
 }
