16143341ebaa46cf1d651020f4ff186ba8d78f26
MichaelFedulov <Mikhail_Fedulov@hotmail.com>
2019-09-01 19:49:47 +1000

    Added Program Files
    index.js and methods.js

---
+++ store/program/index.js
@@ -0,0 +1,92 @@
+import { getField, updateField } from 'vuex-map-fields'
+import { ProgramApi } from '../../core/Api' //need StudentApi
+import {
+  SUCCESS,
+  ERROR,
+  REQUEST,
+  SUBMIT,
+  ADD_BACKGROUND,
+  EDUCATIONAL_BACKGROUND,
+  REMOVE_BACKGROUND
+} from './methods'
+
+const emptyState = () => ({
+  status: '',
+  program: {
+    id: '',
+    skillsetId: '',
+    title: '',
+    targetGroup: '',
+    description: '',
+  }
+})
+
+
+export const state = () => emptyState()
+
+export const getters = {
+  getField
+}
+
+export const mutations = {
+  updateField,
+
+  [EDUCATIONAL_BACKGROUND]: (state, value) => {
+    state.student.educationalBackground = value
+  },
+
+  [REQUEST]: state => {
+    state = emptyState()
+    state.status = 'loading'
+  },
+  [SUCCESS]: (state, student) => {
+    state.status = 'success'
+    state.student = student
+  },
+  [ERROR]: state => {
+    state.status = 'error'
+  },
+  [SUBMIT]: state => {
+    state.status = 'submitting'
+  }
+}
+
+export const actions = {
+  [REQUEST]: ({ commit }, { id }) =>
+    new Promise(async (resolve, reject) => {
+      commit(REQUEST)
+      try {
+        const response = await ProgramApi.getStudent(id)
+        commit(SUCCESS, response.data)
+        resolve(response)
+      } catch (e) {
+        commit(ERROR)
+        reject(e)
+      }
+    }),
+
+  [SUBMIT]: ({ commit, state }) =>
+    new Promise(async (resolve, reject) => {
+      commit(SUBMIT)
+      try {
+        const response = await ProgramApi.updateStudent(state.student)
+        commit(SUCCESS, state.student)
+        resolve(response)
+      } catch (e) {
+        commit(ERROR)
+        reject(e)
+      }
+    }),
+
+  [ADD_BACKGROUND]: ({ commit, state }, { education }) => {
+    const shallowCopy = [...state.program.educationalBackground] //student
+    shallowCopy.push(education)
+    commit(EDUCATIONAL_BACKGROUND, shallowCopy)
+  },
+
+  [REMOVE_BACKGROUND]: ({ commit, state }, { education }) => {
+    const shallowCopy = [...state.program.educationalBackground] //student
+    shallowCopy.splice(shallowCopy.indexOf(education), 1)
+    commit(EDUCATIONAL_BACKGROUND, shallowCopy)
+  }
+}
---
+++ store/program/methods.js
@@ -0,0 +1,22 @@
+import { storeModule } from '../../core/helpers'
+
+const MODULE = 'program'
+
+// mapped fields
+export const ID = 'program.id'
+export const SKILLSETID = 'program.skillsetId'
+export const TITLE = 'program.title'
+export const TRAGETGROUP= 'program.targetGroup'
+export const DESCRIPTION = 'program.description'
+
+// fetching states
+export const REQUEST = 'request'
+export const SUBMIT = 'submit'
+export const SUCCESS = 'success'
+export const ERROR = 'error'
+
+// methods
+export const ADD_BACKGROUND = 'addBackground'
+export const REMOVE_BACKGROUND = 'removeBackground'
+
+export const studentModule = method => storeModule(MODULE, method)
