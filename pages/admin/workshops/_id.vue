90a6c16dc647d1d3f3e2394d5fb3bd7c9607ea9e
Nameer Abdullah Shams <nameerabdullah.shams-1@student.uts.edu.au>
2019-09-03 15:16:54 +1000

    added id on workshop still have to make the page

---
+++ pages/admin/workshops/_id.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="page-workshops">
+    <section class="container">
+      <h1>workshops {{ this.$route.params.id }}</h1>
+    </section>
+  </div>
+</template>
+
+<script>
+import { adminAuthenticated } from '../../../middleware/authenticatedRoutes'
+
+export default {
+  middleware: adminAuthenticated,
+  layout: 'admin'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~assets/styles/variables';
+</style>
