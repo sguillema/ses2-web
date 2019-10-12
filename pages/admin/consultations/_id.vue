7a5fd3ce9661fdf36fbb6f3a6b3563a362da85d5
Sebastian Guillema <sguillema@hotmail.com>
2019-10-12 21:50:51 +1100

    Created Attendance Key Generator component - Updated Sheet component with more options - Added webpack.config for scss module resolution in webstorm

---
+++ pages/admin/consultations/_id.vue
@@ -0,0 +1,19 @@
+<template>
+  <attendance-key-generator />
+</template>
+
+<script>
+import { adminAuthenticated } from '../../../middleware/authenticatedRoutes'
+import AttendanceKeyGenerator from '../../../components/AttedanceKeyGenerator/AttendanceKeyGenerator'
+
+export default {
+  components: { AttendanceKeyGenerator },
+  middleware: adminAuthenticated,
+  layout: 'admin',
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
