a454db2fe043b33b40a28f328b432b12b4d53f79
Hayden Crain <haydencrain@optusnet.com.au>
2019-10-15 13:33:52 +1100

    add breadcrumbs and dynamic title

--- components/Layout/Admin/Header.vue
+++ components/Layout/Admin/Header.vue
@@ -1,15 +1,17 @@
 <template>
   <v-toolbar id="header" flat height="100">
     <v-toolbar-title>
-      <div class="reee">Welcome 12345678.</div>
-      <v-breadcrumbs class="breadcrumbs" :items="items">
+      <div class="reee">
+        {{ getRouteName() }}
+      </div>
+      <v-breadcrumbs class="breadcrumbs" :items="breadcrumbItems">
         <template v-slot:item="props">
-          <a
+          <v-breadcrumbs-item
             :href="props.item.href"
             :class="[props.item.disabled && 'disabled']"
           >
             {{ props.item.text.toUpperCase() }}
-          </a>
+          </v-breadcrumbs-item>
         </template>
       </v-breadcrumbs>
     </v-toolbar-title>
@@ -18,26 +20,62 @@
 </template>
 
 <script>
+import { authModule, USER } from '~/store/auth/methods'
+
 export default {
   data: () => ({
-    items: [
-      {
-        text: 'Dashboard',
-        disabled: false,
-        href: '/admin/dashboard'
-      },
-      {
-        text: 'Placeholder 1',
-        disabled: false,
-        href: 'breadcrumbs_link_1'
-      },
-      {
-        text: 'Placeholder 2',
-        disabled: true,
-        href: 'breadcrumbs_link_2'
+    dashboardBreadcrumb: {
+      text: 'Dashboard',
+      disabled: true,
+      href: '/admin/dashboard'
+    },
+    breadcrumbItems: []
+  }),
+  computed: {
+    user: {
+      get() {
+        return this.$store.getters[authModule(USER)]
       }
-    ]
-  })
+    }
+  },
+  watch: {
+    $route() {
+      console.log('route changed', this.$route)
+      this.buildBreadcrumb()
+    }
+  },
+  mounted() {
+    this.buildBreadcrumb()
+  },
+  methods: {
+    buildBreadcrumb() {
+      let path = this.$route.path.replace('/admin', '')
+      let items = path.split('/').filter(item => item)
+      let breadcrumbItems = items.map((item, index) => ({
+        text: item,
+        disabled: index === items.length - 1,
+        href: this.$route.path.split(item)[0] + item
+      }))
+      if (!(breadcrumbItems[0] && breadcrumbItems[0].text === 'dashboard'))
+        breadcrumbItems.unshift({
+          text: 'dashboard',
+          disabled: !breadcrumbItems.length,
+          href: '/admin/dashboard'
+        })
+      this.breadcrumbItems = breadcrumbItems
+    },
+
+    getRouteName() {
+      if (this.$route.name === 'admin-dashboard') return this.getDashboardName()
+      let routeName = this.$route.name.replace('admin-', '')
+      if (this.$route.params.id)
+        routeName = routeName.replace('-id', ` ${this.$route.params.id}`)
+      return routeName
+    },
+    getDashboardName() {
+      return `Welcome ${this.user.id}.`
+    }
+  }
 }
 </script>
 
@@ -51,5 +89,16 @@ export default {
 .breadcrumbs {
   padding: 0;
   font-size: 13px;
+}
+</style>
+
+<style lang="scss">
+#header {
+  .disabled {
+    a {
+      color: grey;
+    }
+    pointer-events: none;
+  }
 }
 </style>
